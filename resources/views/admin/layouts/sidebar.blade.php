22d3ff795e71f20d0a72ec5f0654e302a4612d83
Nhiem130303 <nhiemnd1303@gmail.com>
2025-02-27 22:47:36 +0700

    layout

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="left-side-menu left-side-menu-detached">
+    <!--- Sidemenu -->
+    <ul class="metismenu side-nav">
+        <li class="side-nav-title side-nav-item bg-primary-lighten text-white"><h6>For Products</h6></li>
+        <li class="side-nav-item">
+            <a href="" class="side-nav-link">
+                <i class="dripicons-chevron-right"></i>
+                <span>Create roles</span>
+            </a>
+        </li>
+    </ul>
+    <!-- End Sidebar -->
+
+    <div class="clearfix"></div>
+    <!-- Sidebar -left -->
+
+</div>
---
+++ resources/views/admin/layouts/topbar.blade.php
@@ -0,0 +1,63 @@
+<div class="navbar-custom topnav-navbar topnav-navbar-dark">
+    <div class="container-fluid">
+
+        <!-- LOGO -->
+        <a href="/admin" class="topnav-logo">
+            <span class="topnav-logo-lg">
+                <img src="{{ asset('images/logo-light.png') }}" alt="" height="16">
+            </span>
+            <span class="topnav-logo-sm">
+                <img src="{{ asset('images/logo_sm.png') }}" alt="" height="16">
+            </span>
+        </a>
+
+        <ul class="list-unstyled topbar-right-menu float-right mb-0">
+            <li class="notification-list">
+                <a class="nav-link right-bar-toggle" href="javascript: void(0);">
+                    <i class="dripicons-gear noti-icon"></i>
+                </a>
+            </li>
+
+            <li class="dropdown notification-list">
+                <a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown" id="topbar-userdrop"
+                    href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    <span class="account-user-avatar">
+                        <img src="{{ asset('images/users/avatar-1.jpg') }}" alt="user-image" class="rounded-circle">
+                    </span>
+                    <span>
+                        <span class="account-user-name">Messi</span>
+                        <span class="account-position">Admin</span>
+                    </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown"
+                    aria-labelledby="topbar-userdrop">
+                    <!-- item-->
+                    <div class=" dropdown-header noti-title">
+                        <h6 class="text-overflow m-0">Welcome !</h6>
+                    </div>
+
+                    <!-- item-->
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i class="mdi mdi-account-circle mr-1"></i>
+                        <span>My Account</span>
+                    </a>
+
+                    <!-- item-->
+                    <a href="/admin/logout" class="dropdown-item notify-item">
+                        <i class="mdi mdi-logout mr-1"></i>
+                        <span>Logout</span>
+                    </a>
+
+                </div>
+            </li>
+
+        </ul>
+        <a class="button-menu-mobile disable-btn">
+            <div class="lines">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </a>
+    </div>
+</div>
