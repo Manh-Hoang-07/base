d69daaefe89bc3637ac5764a146dfa6b7059acd9
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2025-03-06 01:21:53 +0700

    quản lý quyền, vai trò

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,28 +2,30 @@
     <!--- Sidemenu -->
     <ul class="metismenu side-nav">
         <li class="side-nav-title side-nav-item bg-primary-lighten text-white"><h6>For Products</h6></li>
-        @canany(['manage_users', 'view_users', 'create_users', 'edit_users', 'delete_users', 'assign_users'])
-        <li class="side-nav-item">
-            <a href="{{ route('admin.users.index') }}" class="side-nav-link">
-                <i class="dripicons-chevron-right"></i>
-                <span>Quản lý tài khoản</span>
-            </a>
-        </li>
+        @canany(['view_users', 'create_users', 'edit_users', 'delete_users', 'assign_users'])
+            <li class="side-nav-item">
+                <a href="{{ route('admin.users.index') }}" class="side-nav-link">
+                    <i class="dripicons-chevron-right"></i>
+                    <span>Quản lý tài khoản</span>
+                </a>
+            </li>
         @endcan
-        @canany(['manage_roles', 'view_roles', 'create_roles', 'edit_roles', 'delete_roles'])
-        <li class="side-nav-item">
-            <a href="{{ route('admin.roles.index') }}" class="side-nav-link">
-                <i class="dripicons-chevron-right"></i>
-                <span>Quản lý vai trò</span>
-            </a>
-        </li>
+        @canany(['view_roles', 'create_roles', 'edit_roles', 'delete_roles'])
+            <li class="side-nav-item">
+                <a href="{{ route('admin.roles.index') }}" class="side-nav-link">
+                    <i class="dripicons-chevron-right"></i>
+                    <span>Quản lý vai trò</span>
+                </a>
+            </li>
         @endcanany
-        <li class="side-nav-item">
-            <a href="{{ route('admin.permissions.index') }}" class="side-nav-link">
-                <i class="dripicons-chevron-right"></i>
-                <span>Quản lý quyền</span>
-            </a>
-        </li>
+        @canany(['view_permissions', 'create_permissions', 'edit_permissions', 'delete_permissions'])
+            <li class="side-nav-item">
+                <a href="{{ route('admin.permissions.index') }}" class="side-nav-link">
+                    <i class="dripicons-chevron-right"></i>
+                    <span>Quản lý quyền</span>
+                </a>
+            </li>
+        @endcanany
     </ul>
     <!-- End Sidebar -->
 
