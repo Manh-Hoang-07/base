c2d3ea9a084f5cd6a286ce89933e6ff7580e17ab
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2025-03-17 21:53:53 +0700

    Thêm BaseController

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    /**
+     * Hàm dùng chung cho autocomplete
+     *
+     * @param Request $request
+     * @param string|Model $modelClass
+     * @param string $column
+     * @param int $limit
+     * @return JsonResponse
+     */
+    protected function baseAutocomplete(Request $request, string|Model $modelClass = '', string $column = 'title', int $limit = 10): JsonResponse
+    {
+        $term = $request->input('term');
+        // Kiểm tra nếu class tồn tại và là một Model hợp lệ
+        if (is_string($modelClass)
+            && (!class_exists($modelClass) || !is_subclass_of($modelClass, 'Illuminate\Database\Eloquent\Model'))
+        ) {
+            return response()->json(['error' => 'Invalid model class'], 400);
+        }
+        $results = $modelClass::query()
+            ->where($column, 'like', '%' . $term . '%')
+            ->select('id', $column)
+            ->limit($limit)
+            ->get();
+        return response()->json($results);
+    }
+}
