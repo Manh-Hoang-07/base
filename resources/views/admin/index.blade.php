e81143c2157e6d8a184c67fdaaea93ebc011dd3d
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2025-06-12 22:37:14 +0700

    nhanh mới 1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -18,7 +18,6 @@
     <!-- Critical CSS - load synchronously -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('adminlte/css/adminlte.min.css') }}"/>
-    <!-- AdminLTE CSS test -->
 
     <!-- Admin Custom CSS -->
     <style>
